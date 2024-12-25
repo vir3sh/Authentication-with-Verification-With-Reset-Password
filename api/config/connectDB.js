9801f129fb6c5dcd25fba7f378397ebcc5890475
vir3sh <pviresh508@gmail.com>
2024-12-25 11:13:16 +0530

    backend for deployment3

--- api/config/connectDB.js
+++ api/config/connectDB.js
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 const connectDB = async () => {
-  mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000, // Adjust as needed
-    connectTimeoutMS: 10000, // Increase connection timeout
-  });
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Error connecting to MongoDB:", err));
 };
 
 export default connectDB;
