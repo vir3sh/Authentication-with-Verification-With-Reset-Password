7a0d26266a3740f9688db3d7cfd31cd265589707
vir3sh <pviresh508@gmail.com>
2024-12-25 10:20:44 +0530

    backend completed4

--- api/config/connectDB.js
+++ api/config/connectDB.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 import "dotenv/config";
-
 const connectDB = async () => {
-  mongoose
-    .connect("mongodb+srv://pviresh508:@authentication.mw7ob.mongodb.net/")
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log("Error connecting to MongoDB:", err));
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
