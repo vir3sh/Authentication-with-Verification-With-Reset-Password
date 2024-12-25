f1f0cc6a4e180420ad2d5c989131293d4ea80fa5
vir3sh <pviresh508@gmail.com>
2024-12-25 10:30:52 +0530

    backend for deployment

--- api/config/connectDB.js
+++ api/config/connectDB.js
@@ -1,16 +1,11 @@
 import mongoose from "mongoose";
 import "dotenv/config";
+
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Error connecting to MongoDB:", err));
 };
 
 export default connectDB;
